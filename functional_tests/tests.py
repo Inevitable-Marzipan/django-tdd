f8fb84e134f77ae4b944999ae531affc2128b2a3
TimJ <timj@elastacloud.com>
2021-04-06 21:22:53 +0100

    FTs for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,7 +7,6 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
-    
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -85,3 +84,24 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
+    
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        input_box.send_keys('testing')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. testing')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input_box.locaiton['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
