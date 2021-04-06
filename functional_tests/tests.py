d1b258249c18510d83acf3a4208c48dd8ec5fd47
TimJ <timj@elastacloud.com>
2021-04-06 17:53:53 +0100

    New FT for multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -54,3 +54,35 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1. Buy peacock feathers')
         self.wait_for_row_in_list_table('2. Use peacock feathers to make a fly')
         self.fail('Finish the test!')
+    
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
+
+        first_url = self.browser.current_url
+        self.assertRegex(first_url, 'lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Buy milk')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1. Buy milk')
+
+        second_url = self.browser.current_url
+        self.assertRegex(second_url, 'lists/.+')
+        self.assertNotEqual(second_url, first_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Buy milk', page_text)
